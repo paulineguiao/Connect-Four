d9bbbabe1b363c2a67436eb2c1d6f098f81e24af
paulineguiao <allyguiao@gmail.com>
2026-06-16 22:34:16 -0600

    Update Player.cs
    Add Player abstract class

--- Player.cs
+++ Player.cs
@@ -4,7 +4,18 @@ using System.Text;
 
 namespace ConnectFour
 {
-    internal class Player
+    public abstract class Player
     {
+        public string Name { get; set; }
+        public char Symbol { get; set; }
+
+        public Player(string name, char symbol)
+        {
+            Name = name;
+            Symbol = symbol;
+        }
+
+        // Each player must choose a column
+        public abstract int ChooseColumn();
     }
 }
