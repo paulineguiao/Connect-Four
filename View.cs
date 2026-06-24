4caee23ae3533f867972077b7ba0bdc7b3a1cc10
paulineguiao <allyguiao@gmail.com>
2026-06-23 21:12:39 -0600

    Update View.cs

--- View.cs
+++ View.cs
@@ -1,24 +1,45 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
-namespace ConnectFour
-{
-   public class ConsoleView
+    namespace ConnectFour
     {
-        public void ShowWelcome()
+        public class ConsoleView
         {
-            Console.WriteLine("Welcome to Connect Four!");
-        }
+            public void ShowWelcome()
+            {
+                Console.Clear();
+                Console.WriteLine("===============================================");
+                Console.WriteLine("     Connect 4 Game Development Project:       ");
+                Console.WriteLine("===============================================");
+            }
 
-        public void ShowBoard(char[,] grid)
-        {
-            //don't know yet.
-        }
+            public void ShowBoard(char[,] grid)
+            {
+                Console.Clear();
 
-        public void ShowMessage(string message)
-        {
-            Console.WriteLine(message);
+                int rows = grid.GetLength(0);
+                int columns = grid.GetLength(1);
+
+
+                for (int r = 0; r < rows; r++)
+                {
+                    for (int c = 0; c < columns; c++)
+                    {
+
+                        Console.Write("| " + grid[r, c] + " ");
+                    }
+                    Console.WriteLine("|");
+                }
+
+                Console.WriteLine("-----------------------------");
+
+                Console.WriteLine("  1   2   3   4   5   6   7  ");
+            }
+
+            public void ShowMessage(string message)
+            {
+                Console.WriteLine(message);
+            }
         }
     }
-}
